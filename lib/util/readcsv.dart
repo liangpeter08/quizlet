1306f2588e70f177995523ed0b06403662760a82
peter.liang <liangpeter08>
2019-05-10 16:25:47 -0400

    csv

--- lib/util/readcsv.dart
+++ lib/util/readcsv.dart
@@ -1,33 +1,8 @@
-import 'dart:io';
-import 'dart:async';
-import 'dart:convert';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
-readcsv() {
-  final File file = new File('./lib/assets/quiz.csv');
-  print('here');
-  Stream<List> inputStream = file.openRead();
-
-  inputStream
-      .transform(utf8.decoder)       // Decode bytes to UTF-8.
-      .transform(new LineSplitter()) // Convert stream to individual lines.
-      .listen((String line) {        // Process results.
-
-       List row = line.split(','); // split by comma
-
-        String id = row[0];
-        String symbol = row[1];
-        String open = row[2];
-        String low = row[3];
-        String high = row[4];
-        String close = row[5];
-        String volume = row[6];
-        String exchange = row[7];
-        String timestamp = row[8];
-        String date = row[9];
-
-        print('$id, $symbol, $open');
-
-      },
-      onDone: () { print('File is now closed.'); },
-      onError: (e) { print(e.toString()); });
+Future<String> readcsv() async {
+  String a = await rootBundle.loadString('assets/quiz.csv');
+  print(a);
+  return a;
 }
