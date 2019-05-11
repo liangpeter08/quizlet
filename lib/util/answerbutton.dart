bfdb68e8ec581e19478e1642423ca3f284732cfc
justin.lao <qlao@edu.uwaterloo.ca>
2019-05-10 22:06:48 -0400

    shifting between question states and top display
    Peter is a decent coding instructor. On par with Maximillian

--- lib/util/answerbutton.dart
+++ lib/util/answerbutton.dart
@@ -5,27 +5,43 @@ class QandA extends StatelessWidget {
   List<int> answerOrder;
   var btnHandler;
   var selectedButton;
+  bool showCorrect;
 
+  QandA(
+      {this.currentQuestion,
+      this.answerOrder,
+      this.btnHandler,
+      this.selectedButton,
+      this.showCorrect = false});
 
-  QandA({this.currentQuestion, this.answerOrder, this.btnHandler, this.selectedButton});
-  
   @override
   Widget build(BuildContext context) {
     List<Widget> questionAndAnswer = new List<Widget>.generate(4, (i) {
       return Center(
+        widthFactor: 1,
+        heightFactor: 1,
         child: Container(
             child: RaisedButton(
-              color: this.selectedButton == i ? answerOrder[i] == 1 ? Colors.green: Colors.red : Colors.grey,
+          color: this.selectedButton == i
+              ? answerOrder[i] == 1 ? Colors.green : Colors.red
+              : (this.showCorrect && answerOrder[i] == 1)
+                  ? Colors.green
+                  : Colors.grey,
           child: Text(currentQuestion[answerOrder[i]]),
-          onPressed: () => btnHandler(i),
+          onPressed: this.selectedButton == -1
+              ? () => btnHandler(i, answerOrder[i])
+              : () {},
         )),
       );
     });
 
-    questionAndAnswer.insert(0, Center(
-      child: Container(
-          margin: EdgeInsets.only(top: 20.0), child: Text(currentQuestion[0])),
-    ));
-  return ListView(children: questionAndAnswer);
-  } 
+    questionAndAnswer.insert(
+        0,
+        Center(widthFactor: 1, heightFactor: 1,
+          child: Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: Text(currentQuestion[0])),
+        ));
+    return Column(children: questionAndAnswer);
+  }
 }
--- lib/views/questionview.dart
+++ lib/views/questionview.dart
@@ -12,13 +12,15 @@ class QuestionPage extends StatefulWidget {
   int selectedAnswer;
   List<int> answerOrder;
   List<String> currentQuestion;
+  bool showCorrect;
 
   QuestionPage(
       {this.questions,
       this.selectedQuestions,
       this.index = 0,
       this.mistakes = 0,
-      this.selectedAnswer = -1}) {
+      this.selectedAnswer = -1,
+      this.showCorrect = false}) {
     answerOrder = generateOrder(4);
     currentQuestion = this.questions[this.selectedQuestions[this.index]];
   }
@@ -29,16 +31,37 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionState extends State<QuestionPage> {
-  onClick(int selected) {
-    // print('okay' + selected.toString());
-    // print(widget.questions[1][1]);
-    // if (selected == 1) {
-
-    // }
-
+  onClick(int selected, int csvColNum) async {
     setState(() {
       widget.selectedAnswer = selected;
     });
+    await new Future.delayed(const Duration(seconds: 1));
+    if (csvColNum != 1) {
+      setState(() {
+        widget.showCorrect = true;
+        ++widget.mistakes;
+      });
+    }
+    await new Future.delayed(const Duration(seconds: 3));
+
+    if (widget.index + 1 < widget.selectedQuestions.length) {
+      setState(() {
+        ++widget.index;
+        widget.selectedAnswer = -1;
+        widget.answerOrder = generateOrder(4);
+        widget.currentQuestion =
+            widget.questions[widget.selectedQuestions[widget.index]];
+        widget.showCorrect = false;
+      });
+    }
+  }
+  
+  String displayMistakes(int mistakes) {
+    String strikes = '';
+    for (int i = 0; i < mistakes; ++i) {
+      strikes += 'X';
+    }
+    return strikes;
   }
 
   Widget build(BuildContext context) {
@@ -48,10 +71,22 @@ class _QuestionState extends State<QuestionPage> {
           // the App.build method, and use it to set our appbar title.
           title: Text('Quizlett'),
         ),
-        body: QandA(
+        body: Column(children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(margin: EdgeInsets.all(20.0), child: Text('Strikes: ${displayMistakes(widget.mistakes)}', style: TextStyle(fontSize: 20.0))),
+              Expanded(child: Container(margin: EdgeInsets.all(20.0), child: Align(
+                  alignment: Alignment.topRight,
+                  child: Text('Total: ${widget.index + 1}/${widget.selectedQuestions.length}', style: TextStyle(fontSize: 20.0)))))
+            ],
+          ),
+          QandA(
             currentQuestion: widget.currentQuestion,
             answerOrder: widget.answerOrder,
             btnHandler: onClick,
-            selectedButton: widget.selectedAnswer));
+            selectedButton: widget.selectedAnswer,
+            showCorrect: widget.showCorrect,
+          )
+        ]));
   }
 }
