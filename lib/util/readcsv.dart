c3978f375d578fc44b6c8c5d6725b357eb2ff032
justin.lao <qlao@edu.uwaterloo.ca>
2019-05-17 00:35:43 -0400

    new scrapping script and Ontario questions

--- lib/util/readcsv.dart
+++ lib/util/readcsv.dart
@@ -4,6 +4,6 @@ import 'package:flutter/services.dart' show rootBundle;
 Future<List<List<String>>> readcsv() async {
   String a = await rootBundle.loadString('assets/quiz.csv');
   List<String> questions = a.split('\n');
-  List<List<String>> items = questions.map((questions) => questions.split(',')).toList();
+  List<List<String>> items = questions.map((questions) => questions.split('*!*')).toList();
   return items;
 }
