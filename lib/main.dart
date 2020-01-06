e3317758ec44cc976bc5fa9be33469f42ddb703a
Peter <liangpeter08@gmail.com>
2020-01-05 20:57:12 -0500

    version 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,10 @@ import 'package:flutter/material.dart';
 import './views/mainView/mainview.dart';
 
 void main() async {
-  // FacebookAudienceNetwork.init(
-  //   // testingId: '746e1920-e104-4f2e-9ff7-3eb8d32f2f2b',
-  //     testingId: '0c8948b7-d568-4554-a471-0f23bffc04f7',
-  // );
+  FacebookAudienceNetwork.init(
+    // testingId: '746e1920-e104-4f2e-9ff7-3eb8d32f2f2b',
+      testingId: '0c8948b7-d568-4554-a471-0f23bffc04f7',
+  );
   return runApp(MaterialApp(
     title: 'Canadian Citizenship Test 2020',
     theme: ThemeData(
